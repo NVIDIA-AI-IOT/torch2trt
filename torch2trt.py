5af20cb1d0040e4c1fee5bc1465ce7b893da2136
John Welsh <jwelsh@nvidia.com>
2019-05-02 12:48:52 -0700

    added conv2d transpose. does not support output_padding > 0

--- torch2trt.py
+++ torch2trt.py
@@ -283,6 +283,45 @@ def convert_Conv2d(ctx):
 
     output._trt = layer.get_output(0)
 
+    
+@tensorrt_converter('torch.nn.ConvTranspose2d.forward')
+def convert_ConvTranspose2d(ctx):
+    module = ctx.method_args[0]
+    input = ctx.method_args[1]
+    output = ctx.method_return
+
+    kernel_size = module.kernel_size
+    if not isinstance(kernel_size, tuple):
+        kernel_size = (kernel_size, ) * 2
+
+    stride = module.stride
+    if not isinstance(stride, tuple):
+        stride = (stride, ) * 2
+
+    padding = module.padding
+    if not isinstance(padding, tuple):
+        padding = (padding, ) * 2
+        
+    kernel = module.weight.detach().cpu().numpy()
+    
+    bias = trt.Weights(torch_dtype_to_trt(module.weight.dtype))
+    if module.bias is not None:
+        bias = module.bias.detach().cpu().numpy()
+
+    layer = ctx.network.add_deconvolution(
+        input=input._trt,
+        num_output_maps=module.out_channels,
+        kernel_shape=kernel_size,
+        kernel=kernel,
+        bias=bias)
+    layer.stride = stride
+    layer.padding = padding
+    
+    if module.groups is not None:
+        layer.num_groups = module.groups
+
+    output._trt = layer.get_output(0)
+
 
 @tensorrt_converter('torch.nn.MaxPool2d.forward')
 def convert_MaxPool2d(ctx):
