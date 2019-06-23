ed9e40f34cfa35b4c697e2b949cede63a8a83a82
John Welsh <jwelsh@nvidia.com>
2019-06-23 19:46:21 -0400

    added Conv2d unit tests

--- torch2trt/converters/Conv2d.py
+++ torch2trt/converters/Conv2d.py
@@ -1,4 +1,5 @@
 from torch2trt.torch2trt import *
+from torch2trt.module_test import add_module_test
 
 
 @tensorrt_converter('torch.nn.Conv2d.forward')
@@ -19,6 +20,10 @@ def convert_Conv2d(ctx):
     if not isinstance(padding, tuple):
         padding = (padding, ) * 2
 
+    dilation = module.dilation
+    if not isinstance(dilation, tuple):
+        dilation = (dilation, ) * 2
+
     kernel = module.weight.detach().cpu().numpy()
     
     bias = trt.Weights(torch_dtype_to_trt(module.weight.dtype))
@@ -33,8 +38,30 @@ def convert_Conv2d(ctx):
         bias=bias)
     layer.stride = stride
     layer.padding = padding
+    layer.dilation = dilation
 
     if module.groups is not None:
         layer.num_groups = module.groups
 
     output._trt = layer.get_output(0)
+
+
+
+@add_module_test(torch.float32, torch.device('cuda'), [(1, 10, 224, 224)])
+def test_Conv2d_basic():
+    return torch.nn.Conv2d(10, 5, kernel_size=1, stride=1, padding=0)
+
+
+@add_module_test(torch.float32, torch.device('cuda'), [(1, 10, 224, 224)])
+def test_Conv2d_stride2():
+    return torch.nn.Conv2d(10, 5, kernel_size=1, stride=2, padding=0)
+
+
+@add_module_test(torch.float32, torch.device('cuda'), [(1, 10, 224, 224)])
+def test_Conv2d_kernel3():
+    return torch.nn.Conv2d(10, 5, kernel_size=3, stride=2, padding=1)
+
+
+@add_module_test(torch.float32, torch.device('cuda'), [(1, 10, 224, 224)])
+def test_Conv2d_dilation2():
+    return torch.nn.Conv2d(10, 5, kernel_size=3, stride=1, padding=1, dilation=2)
