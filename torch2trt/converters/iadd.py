678aa20f31bd4b98bac2d66a9ac1ad0aae45d564
John Welsh <jwelsh@nvidia.com>
2019-06-23 22:51:02 -0400

    moved iadd to separate file

--- torch2trt/converters/iadd.py
+++ torch2trt/converters/iadd.py
@@ -2,22 +2,23 @@ from torch2trt.torch2trt import *
 from torch2trt.module_test import add_module_test
 
 
-@tensorrt_converter('torch.Tensor.__add__')
-def convert_add(ctx):
+@tensorrt_converter('torch.Tensor.__iadd__')
+def convert_iadd(ctx):
     input_a = ctx.method_args[0]
     input_b = ctx.method_args[1]
-    output = ctx.method_return
     layer = ctx.network.add_elementwise(input_a._trt, input_b._trt, trt.ElementWiseOperation.SUM)
-    output._trt = layer.get_output(0)
+    ctx.method_args[0]._trt = layer.get_output(0)
 
 
-class Add(torch.nn.Module):
+class IAdd(torch.nn.Module):
     def __init__(self):
-        super(Add, self).__init__()
+        super(IAdd, self).__init__()
 
     def forward(self, x, y):
-        return x + y
+        x += y
+        return x
+
 
 @add_module_test(torch.float32, torch.device('cuda'), [(1, 3, 224, 224), (1, 3, 224, 224)])
-def test_add_basic():
-    return Add()
+def test_iadd_basic():
+    return IAdd()
