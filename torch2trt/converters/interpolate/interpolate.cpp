2809669555ff96e9e17b39694feb12a4dabce72c
John Welsh <jwelsh@nvidia.com>
2019-06-25 15:01:36 -0400

    asdf

--- torch2trt/converters/interpolate/interpolate.cpp
+++ torch2trt/converters/interpolate/interpolate.cpp
@@ -1,6 +1,9 @@
+#include <iostream>
 #include <NvInfer.h>
 #include <ATen/ATen.h>
+#include <ATen/cuda/CUDAEvent.h>
 #include <torch/torch.h>
+#include <cuda_runtime_api.h>
 #include "interpolate.pb.h"
 
 
@@ -90,10 +93,32 @@ public:
     at::Tensor input = at::from_blob((void*) inputs[0], input_sizes, [](void*){}, tensor_options);
     at::Tensor output = at::from_blob(outputs[0], input_sizes, [](void*){}, tensor_options);
 
-    // execute
-    if (message.mode() == "nearest") {
+    // create new torch cuda stream
+    at::cuda::CUDAStream torch_stream = at::cuda::getStreamFromPool();
+    at::cuda::CUDAStreamGuard torch_guard(torch_stream);
+
+    // capture current work on tensorrt cuda stream
+    cudaEvent_t event;
+    cudaEventCreate(&event);
+    cudaEventRecord(event, stream);
+
+    // make torch cuda stream wait on tensorrt work
+    cudaStreamWaitEvent(torch_stream.stream(), event, 0);
+
+    // enqueue work
+    if (message.mode() == "bilinear") {
       at::upsample_bilinear2d_out(output, input, {output_sizes[2], output_sizes[3]}, message.align_corners());
     }
+
+    // capture event on enqueued stream
+    cudaEvent_t torch_event;
+    cudaEventCreate(&torch_event);
+    cudaEventRecord(torch_event, torch_stream.stream());
+
+    cudaStreamWaitEvent(stream, torch_event, 0);
+
+    cudaEventDestroy(event);
+    cudaEventDestroy(torch_event);
 
     return 0;
   }
@@ -120,4 +145,42 @@ public:
 
 };
 
+class interpolate_PluginCreator : public IPluginCreator {
+public:
+  interpolate_PluginCreator() {}
+
+  const char *getPluginNamespace() const override {
+    return "torch2trt";
+  }
+
+  const char *getPluginName() const override {
+    return "interpolate";
+  }
+
+  const char *getPluginVersion() const override {
+    return "1";
+  }
+
+  IPluginV2 *deserializePlugin(const char *name, const void *data, size_t length) override {
+    interpolate_message message;
+    message.ParseFromArray(data, length);
+    return new interpolate_plugin(message);
+  }
+
+  void setPluginNamespace(const char *N) override {}
+  const PluginFieldCollection *getFieldNames() override { return nullptr; }
+
+  IPluginV2 *createPlugin(const char *name, const PluginFieldCollection *fc) override { return nullptr; }
+
+};
+
+REGISTER_TENSORRT_PLUGIN(interpolate_PluginCreator);
+
+}
+
+int main() {
+
+  interpolate_message m;
+  std::cout << m.size_size() << std::endl;
+  return 0;
 }
