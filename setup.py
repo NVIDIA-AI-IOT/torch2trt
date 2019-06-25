7d2bdf02ce1f5b78f77a8209f7779fcfe8f96037
John Welsh <jwelsh@nvidia.com>
2019-06-25 18:43:02 -0400

    plugins

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup, find_packages
+from build import build
+
+try:
+    build()
+except e:
+    print('Could not build plugins')
 
 setup(
     name='torch2trt',
     version='0.0',
     description='PyTorch to TensorRT converter',
     packages=find_packages(),
+    package_data={'torch2trt': ['libtorch2trt.so']}
 )
