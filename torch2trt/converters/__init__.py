65b9e6502540177bf009d363978de403bc6e9e39
GeoffreyChen <geoffreychen777@gmail.com>
2019-07-24 19:30:37 +0800

    add div

--- torch2trt/converters/__init__.py
+++ torch2trt/converters/__init__.py
@@ -2,6 +2,7 @@ from .adaptive_avg_pool2d import *
 from .AdaptiveAvgPool2d import *
 from .add import *
 from .mul import *
+from .div import *
 from .iadd import *
 from .AvgPool2d import *
 from .BatchNorm2d import *
