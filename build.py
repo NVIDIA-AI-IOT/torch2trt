f7f60d81675494c07ecb5be37d45be2235d4d507
John Welsh <jwelsh@nvidia.com>
2019-08-06 21:20:14 -0400

    new build

--- build.py
+++ build.py
@@ -2,61 +2,19 @@ import imp
 import subprocess
 import os
 from string import Template
-
-PLUGINS = [
-    'torch_plugin',
-    'interpolate',
-]
-
-BASE_FOLDER = 'torch2trt/converters'
-
-NINJA_STR = Template(
-"""
-rule link
-  command = g++ -g -shared -o $$out $$in -L$torch_dir/lib -L$cuda_dir/lib64 -lc10 -lc10_cuda -ltorch -lcudart -lcaffe2 -lcaffe2_gpu -lprotobuf -lprotobuf-lite -pthread -lpthread -lnvinfer
-
-rule protoc
-  command = protoc $$in --cpp_out=. --python_out=.
-
-rule cxx
-  command = g++ -g -c -fPIC $$in -I$cuda_dir/include -I$torch_dir/include -I$torch_dir/include/torch/csrc/api/include -I. 
-
-"""
-).substitute({
-    'torch_dir': imp.find_module('torch')[1],
-    'cuda_dir': '/usr/local/cuda'
-})
-
-PLUGIN_TEMPLATE = Template(
-"""
-build $plugin_dir/$plugin.pb.h $plugin_dir/$plugin.pb.cc $plugin_dir/${plugin}_pb2.py: protoc $plugin_dir/$plugin.proto
-build $plugin.pb.o: cxx $plugin_dir/$plugin.pb.cc
-build $plugin.o: cxx $plugin_dir/$plugin.cpp
-"""
-)
-
+import sys
+sys.path.append('torch2trt')
+import torch2trt.build_utils
 
 def build():
-    global PLUGINS, BASE_FOLDER, NINJA_STR, PLUGIN_TEMPLATE
-    plugin_o_files = []
-    for plugin in PLUGINS:
-        NINJA_STR += \
-            PLUGIN_TEMPLATE.substitute({
-                'plugin': plugin,
-                'plugin_dir': os.path.join(BASE_FOLDER, plugin),
-            })
-        plugin_o_files += [plugin + '.pb.o', plugin + '.o']
-
-    NINJA_STR += Template(
-"""
-build torch2trt/libtorch2trt.so: link $o_files
-"""
-    ).substitute({'o_files': ' '.join(plugin_o_files)})
-
-    with open('build.ninja', 'w') as f:
-        f.write(NINJA_STR)
-
-    subprocess.call(['ninja'])
-
+    torch2trt.build_utils.build_library(
+        name='torch2trt_plugins', 
+        srcs=['torch2trt/plugins/torch_plugin.cpp'], 
+        protos=['torch2trt/plugins/torch_plugin.proto'], 
+        include_dirs=torch2trt.build_utils.torch2trt_dep_include_dirs(), 
+        library_dirs=torch2trt.build_utils.torch2trt_dep_library_dirs(), 
+        libraries=torch2trt.build_utils.torch2trt_dep_libraries()
+    )
+    
 if __name__ == '__main__':
     build()
