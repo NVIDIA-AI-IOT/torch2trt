fc33e5dfa605df821302abc0a2beb1844c78b3a1
John Welsh <jwelsh@nvidia.com>
2019-09-17 13:19:27 -0700

    added additional mul interfaces

--- torch2trt/converters/mul.py
+++ torch2trt/converters/mul.py
@@ -2,6 +2,8 @@ from torch2trt.torch2trt import *
 from torch2trt.module_test import add_module_test
 
 
+@tensorrt_converter('torch.mul')
+@tensorrt_converter('torch.Tensor.__imul__')
 @tensorrt_converter('torch.Tensor.__mul__')
 def convert_mul(ctx):
     input_a = ctx.method_args[0]
@@ -21,3 +23,30 @@ class Mul(torch.nn.Module):
 @add_module_test(torch.float32, torch.device('cuda'), [(1, 3, 224, 224), (1, 3, 224, 224)])
 def test_mul_basic():
     return Mul()
+
+
+class IMul(torch.nn.Module):
+    def __init__(self):
+        super(IMul, self).__init__()
+
+    def forward(self, x, y):
+        x *= y
+        return x
+
+
+@add_module_test(torch.float32, torch.device('cuda'), [(1, 3, 224, 224), (1, 3, 224, 224)])
+def test_mul_imul():
+    return IMul()
+
+
+class TorchMul(torch.nn.Module):
+    def __init__(self):
+        super(TorchMul, self).__init__()
+
+    def forward(self, x, y):
+        return torch.mul(x, y)
+    
+
+@add_module_test(torch.float32, torch.device('cuda'), [(1, 3, 224, 224), (1, 3, 224, 224)])
+def test_mul_torchmul():
+    return TorchMul()
