eed1423cb487ddd7f5738f606c371196891c9141
John Welsh <jwelsh@nvidia.com>
2019-10-04 00:39:51 -0700

    more relu interfaces

--- torch2trt/converters/relu.py
+++ torch2trt/converters/relu.py
@@ -2,7 +2,10 @@ from torch2trt.torch2trt import *
 from .ReLU import *
 
 
+@tensorrt_converter('torch.relu')
+@tensorrt_converter('torch.relu_')
 @tensorrt_converter('torch.nn.functional.relu')
+@tensorrt_converter('torch.nn.functional.relu_')
 def convert_relu(ctx):
     ctx.method_args = (torch.nn.ReLU(),) + ctx.method_args
     convert_ReLU(ctx)
