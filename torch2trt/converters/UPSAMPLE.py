146bd91dccbe6893d5f8ad47e989353026f9881a
Kshitij Srivastava <kshitij.srivastava@uber.com>
2019-11-15 12:08:46 -0500

    working upsample with trt6

--- torch2trt/converters/UPSAMPLE.py
+++ torch2trt/converters/UPSAMPLE.py
@@ -8,25 +8,35 @@ def convert_upsample(ctx):
     input_trt = trt_(ctx.network, input)
     output = ctx.method_return
 
-    shape = module.size
-    if not isinstance(shape, tuple):
-        shape = (shape, ) * 2
-
-    scales = module.scale_factor
-    if not isinstance(scales, tuple):
-        scales = (scales, ) * 2
-
-    resize_mode = module.mode
-    align_corners = module.align_corners
-
     layer = ctx.network.add_resize(
             input=input_trt)
 
-    layer.shape = shape
-    layer.scales = scales
-    layer.resize_mode=resize_mode
-    layer.align_corners = align_corners
+    shape = module.size
+    if shape != None:
+        if isinstance(shape, list):
+            if len(shape) == 2:
+                shape  = (1,shape[0],shape[1])
+            if len(shape) == 3:
+                shape = tuple(shape)
+        else:
+            shape = (1,shape,shape)
+        layer.shape = shape
 
+    scales = module.scale_factor
+    if scales != None:
+        if not isinstance(scales, tuple):
+            scales = (1,scales,scales )
+        layer.scales = scales
+
+    resize_mode = module.mode
+    if resize_mode.lower() in ["linear","bilinear","trilinear"]:
+        layer.resize_mode = trt.ResizeMode.LINEAR
+    else:
+        layer.resize_mode=trt.ResizeMode.NEAREST
+
+    align_corners = module.align_corners
+    if align_corners != None:
+        layer.align_corners = align_corners
     output._trt = layer.get_output(0)
 
 
@@ -36,7 +46,7 @@ def test_nearest_mode():
 
 @add_module_test(torch.float32, torch.device('cuda'), [(1,1,2,2)])
 def test_bilinear_mode():
-    return torch.nn.Upsample(scale_factor=2, mode="bilinear")
+    return torch.nn.Upsample(scale_factor=2, mode="bilinear",align_corners=False)
 
 @add_module_test(torch.float32, torch.device('cuda'), [(1,1,2,2)])
 def test_align_corner():
@@ -44,12 +54,13 @@ def test_align_corner():
 
 @add_module_test(torch.float32, torch.device('cuda'), [(1,1,3,3)])
 def test_bilinear_mode_odd_input_shape():
-    return torch.nn.Upsample(scale_factor=2,mode="bilinear")
+    return torch.nn.Upsample(scale_factor=2,mode="bilinear",align_corners=False)
 
 @add_module_test(torch.float32, torch.device('cuda'), [(1,1,2,2)])
 def test_size_parameter():
-    return torch.nn.Upsample(size=6,mode="bilinear")
-   
+    return torch.nn.Upsample(size=3,mode="nearest")
+
 @add_module_test(torch.float32, torch.device('cuda'), [(1,1,3,3)])
 def test_size_parameter_odd_input():
-    return torch.nn.Upsample(size=6,mode="bilinear")
+    return torch.nn.Upsample(size=6,mode="bilinear",align_corners=False)
+
