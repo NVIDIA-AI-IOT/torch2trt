4817b4bc296e69f10c6dc9259974507efb195705
Michael Pang <michael3795@gmail.com>
2020-01-08 19:26:59 -0600

    Update relu.py

--- torch2trt/converters/relu.py
+++ torch2trt/converters/relu.py
@@ -1,5 +1,14 @@
 from torch2trt.torch2trt import *
-from .ReLU import *
+
+
+@tensorrt_converter('torch.nn.ReLU.forward')
+def convert_ReLU(ctx):
+    input = ctx.method_args[1]
+    input_trt = trt_(ctx.network, input)
+    output = ctx.method_return
+    layer = ctx.network.add_activation(
+        input=input_trt, type=trt.ActivationType.RELU)
+    output._trt = layer.get_output(0)
 
 
 @tensorrt_converter('torch.relu')
@@ -9,3 +18,4 @@ from .ReLU import *
 def convert_relu(ctx):
     ctx.method_args = (torch.nn.ReLU(),) + ctx.method_args
     convert_ReLU(ctx)
+    
