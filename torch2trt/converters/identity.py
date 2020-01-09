841eccad5b680b55ecd35b33dc3e0c6e7114c9f1
Michael Pang <michael3795@gmail.com>
2020-01-08 19:25:40 -0600

    Update identity.py

--- torch2trt/converters/identity.py
+++ torch2trt/converters/identity.py
@@ -10,3 +10,13 @@ def convert_identity(ctx):
     input_trt = trt_(ctx.network, input)
     output = ctx.method_return
     output._trt = input_trt
+
+    
+@tensorrt_converter('torch.nn.Dropout.forward')
+@tensorrt_converter('torch.nn.Dropout2d.forward')
+@tensorrt_converter('torch.nn.Dropout3d.forward')
+def convert_Identity(ctx):
+    input = ctx.method_args[1]
+    input_trt = trt_(ctx.network, input)
+    output = ctx.method_return
+    output._trt = input_trt
