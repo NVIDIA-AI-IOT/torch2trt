477a6a163f5f8096299520cf83a20a31d33707df
Oliver Batchelor <saulzar@gmail.com>
2020-01-31 00:12:50 +1300

    Add 3d convolutions and interpolations

--- torch2trt/converters/upsample.py
+++ torch2trt/converters/upsample.py
@@ -10,6 +10,8 @@ def convert_interpolate(ctx):
     mode = get_arg(ctx, 'mode', pos=3, default='nearest')
     align_corners = get_arg(ctx, 'align_corners', pos=4, default=None)
 
+    input_dim = input.dim() - 2
+    
     input_trt = trt_(ctx.network, input)
     output = ctx.method_return
 
@@ -18,18 +20,15 @@ def convert_interpolate(ctx):
     shape = size
     if shape != None:
         if isinstance(shape, list):
-            if len(shape) == 2:
-                shape  = (1,shape[0],shape[1])
-            if len(shape) == 3:
-                shape = tuple(shape)
+           shape  = (1,) + tuple(shape)
         else:
-            shape = (1,shape,shape)
+            shape = (1,) + (shape,) * input_dim
         layer.shape = shape
 
     scales = scale_factor
     if scales != None:
         if not isinstance(scales, tuple):
-            scales = (1,scales,scales )
+            scales = (1,) + (scales,) * input_dim
         layer.scales = scales
 
     resize_mode = mode
@@ -43,26 +42,51 @@ def convert_interpolate(ctx):
 
     output._trt = layer.get_output(0)
 
-@add_module_test(torch.float32, torch.device('cuda'), [(1,1,2,2)])
+@add_module_test(torch.float32, torch.device('cuda'), [(1,2,12,12)])
 def test_nearest_mode():
     return torch.nn.Upsample(scale_factor=2, mode="nearest")
 
-@add_module_test(torch.float32, torch.device('cuda'), [(1,1,2,2)])
+@add_module_test(torch.float32, torch.device('cuda'), [(1,4,12,12)])
 def test_bilinear_mode():
-    return torch.nn.Upsample(scale_factor=2, mode="bilinear",align_corners=False)
+    return torch.nn.Upsample(scale_factor=3, mode="bilinear",align_corners=False)
 
-@add_module_test(torch.float32, torch.device('cuda'), [(1,1,2,2)])
+@add_module_test(torch.float32, torch.device('cuda'), [(1,3,12,12)])
 def test_align_corner():
     return torch.nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True)
 
-@add_module_test(torch.float32, torch.device('cuda'), [(1,1,3,3)])
+@add_module_test(torch.float32, torch.device('cuda'), [(1,5,13,13)])
 def test_bilinear_mode_odd_input_shape():
     return torch.nn.Upsample(scale_factor=2,mode="bilinear",align_corners=False)
 
-@add_module_test(torch.float32, torch.device('cuda'), [(1,1,2,2)])
+@add_module_test(torch.float32, torch.device('cuda'), [(1,4,12,12)])
 def test_size_parameter():
     return torch.nn.Upsample(size=3,mode="nearest")
 
-@add_module_test(torch.float32, torch.device('cuda'), [(1,1,3,3)])
+@add_module_test(torch.float32, torch.device('cuda'), [(1,2,13,13)])
 def test_size_parameter_odd_input():
     return torch.nn.Upsample(size=[6,3],mode="nearest")
+
+
+@add_module_test(torch.float32, torch.device('cuda'), [(1,1,12,12,12)])
+def test_nearest_mode_3d():
+    return torch.nn.Upsample(scale_factor=2, mode="nearest")
+
+@add_module_test(torch.float32, torch.device('cuda'), [(1,1,12,12,12)])
+def test_bilinear_mode_3d():
+    return torch.nn.Upsample(scale_factor=3, mode="trilinear",align_corners=False)
+
+@add_module_test(torch.float32, torch.device('cuda'), [(1,1,12,12,12)])
+def test_align_corner_3d():
+    return torch.nn.Upsample(scale_factor=4, mode="trilinear", align_corners=True)
+
+@add_module_test(torch.float32, torch.device('cuda'), [(1,1,13,13,13)])
+def test_bilinear_mode_odd_input_shape_3d():
+    return torch.nn.Upsample(scale_factor=2, mode="trilinear",align_corners=False)
+
+@add_module_test(torch.float32, torch.device('cuda'), [(1,1,12,12,12)])
+def test_size_parameter_3d():
+    return torch.nn.Upsample(size=3,mode="trilinear", align_corners=True)
+
+@add_module_test(torch.float32, torch.device('cuda'), [(1,1,13,13,13)])
+def test_size_parameter_odd_input_3d():
+    return torch.nn.Upsample(size=[6,3,3],mode="trilinear", align_corners=False)
