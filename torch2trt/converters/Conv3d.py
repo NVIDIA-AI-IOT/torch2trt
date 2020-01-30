9e96a53c633f19e7abd407dc22220d7653ee9d34
Oliver Batchelor <saulzar@gmail.com>
2020-01-31 00:12:50 +1300

    Add 3d convolutions and interpolations

---
+++ torch2trt/converters/Conv3d.py
@@ -0,0 +1,68 @@
+from torch2trt.torch2trt import *
+from torch2trt.module_test import add_module_test
+
+
+@tensorrt_converter('torch.nn.Conv3d.forward')
+def convert_Conv3d(ctx):
+    module = ctx.method_args[0]
+    input = ctx.method_args[1]
+    input_trt = trt_(ctx.network, input)
+    output = ctx.method_return
+
+    kernel_size = module.kernel_size
+    if not isinstance(kernel_size, tuple):
+        kernel_size = (kernel_size, ) * 3
+
+    stride = module.stride
+    if not isinstance(stride, tuple):
+        stride = (stride, ) * 3
+
+    padding = module.padding
+    if not isinstance(padding, tuple):
+        padding = (padding, ) * 3
+
+    dilation = module.dilation
+    if not isinstance(dilation, tuple):
+        dilation = (dilation, ) * 3
+
+    kernel = trt.Weights(module.weight.detach().cpu().numpy())
+    
+    bias = trt.Weights(torch_dtype_to_trt(module.weight.dtype))
+    if module.bias is not None:
+        bias = trt.Weights(module.bias.detach().cpu().numpy())
+
+
+    layer = ctx.network.add_convolution_nd(input_trt, module.out_channels,
+        trt.Dims(kernel_size), kernel, bias)
+    layer.stride_nd = stride
+    layer.padding_nd = padding
+    layer.dilation_nd = dilation
+
+    if module.groups is not None:
+        layer.num_groups = module.groups
+
+    output._trt = layer.get_output(0)
+
+
+
+@add_module_test(torch.float32, torch.device('cuda'), [(1, 10, 64, 64, 64)])
+def test_Conv3d_basic():
+    return torch.nn.Conv3d(10, 5, kernel_size=1, stride=1, padding=0)
+
+
+@add_module_test(torch.float32, torch.device('cuda'), [(1, 10, 64, 64, 64)])
+def test_Conv3d_stride2():
+    return torch.nn.Conv3d(10, 5, kernel_size=1, stride=2, padding=0)
+
+
+@add_module_test(torch.float32, torch.device('cuda'), [(1, 10, 64, 64, 64)])
+def test_Conv3d_kernel3():
+    return torch.nn.Conv3d(10, 5, kernel_size=3, stride=2, padding=1)
+
+
+@add_module_test(torch.float32, torch.device('cuda'), [(1, 10, 64, 64, 64)])
+def test_Conv3d_dilation2():
+    return torch.nn.Conv3d(10, 5, kernel_size=3, stride=1, padding=1, dilation=2)
+
+
+
