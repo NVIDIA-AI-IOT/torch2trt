22e1118426ca16463d57b97000798e9debc853ee
John Welsh <jwelsh@nvidia.com>
2020-06-11 19:20:39 +0000

    added ONNX conversion alternative to torch2trt

--- torch2trt/test.py
+++ torch2trt/test.py
@@ -19,6 +19,7 @@ def run(self):
     for shape in self.input_shapes:
         inputs_conversion += (torch.zeros(shape).to(self.device).type(self.dtype), )
         
+    
     # convert module
     module_trt = torch2trt(module, inputs_conversion, max_workspace_size=1 << 20,  **self.torch2trt_kwargs)
 
@@ -98,11 +99,13 @@ if __name__ == '__main__':
     parser.add_argument('--name', help='Regular expression to filter modules to test by name', type=str, default='.*')
     parser.add_argument('--tolerance', help='Maximum error to print warning for entry', type=float, default='-1')
     parser.add_argument('--include', help='Addition python file to include defining additional tests', action='append', default=[])
+    parser.add_argument('--use_onnx', help='Whether to test using ONNX or torch2trt tracing', action='store_true')
     args = parser.parse_args()
     
     for include in args.include:
         runpy.run_module(include)
         
+    num_tests, num_success, num_tolerance, num_error = 0, 0, 0, 0
     for test in MODULE_TESTS:
         
         # filter by module name
@@ -110,16 +113,32 @@ if __name__ == '__main__':
         if not re.search(args.name, name):
             continue
             
+        num_tests += 1
         # run test
-        max_error, fps, fps_trt, ms, ms_trt = run(test)
+        try:
+            if args.use_onnx:
+                test.torch2trt_kwargs.update({'use_onnx': True})
+                
+            max_error, fps, fps_trt, ms, ms_trt = run(test)
+
+            # write entry
+            line = '| %s | %s | %s | %s | %.2E | %.3g | %.3g | %.3g | %.3g |' % (name, test.dtype.__repr__().split('.')[-1], str(test.input_shapes), str(test.torch2trt_kwargs), max_error, fps, fps_trt, ms, ms_trt)
         
-        # write entry
-        line = '| %s | %s | %s | %s | %.2E | %.3g | %.3g | %.3g | %.3g |' % (name, test.dtype.__repr__().split('.')[-1], str(test.input_shapes), str(test.torch2trt_kwargs), max_error, fps, fps_trt, ms, ms_trt)
-
-        if args.tolerance >= 0 and max_error > args.tolerance:
-            print(colored(line, 'yellow'))
-        else:
-            print(line)
-
+            if args.tolerance >= 0 and max_error > args.tolerance:
+                print(colored(line, 'yellow'))
+                num_tolerance += 1
+            else:
+                print(line)
+            num_success += 1
+        except:
+            line = '| %s | %s | %s | %s | N/A | N/A | N/A | N/A | N/A |' % (name, test.dtype.__repr__().split('.')[-1], str(test.input_shapes), str(test.torch2trt_kwargs))
+            print(colored(line, 'red'))
+            num_error += 1
+            
         with open(args.output, 'a+') as f:
             f.write(line + '\n')
+    
+    print('NUM_TESTS: %d' % num_tests)
+    print('NUM_SUCCESSFUL_CONVERSION: %d' % num_success)
+    print('NUM_FAILED_CONVERSION: %d' % num_error)
+    print('NUM_ABOVE_TOLERANCE: %d' % num_tolerance)
