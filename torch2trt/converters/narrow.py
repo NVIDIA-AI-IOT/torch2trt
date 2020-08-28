ee2ecd4af810fbdd5e1c05d3925efffe8f4ed88c
Kshitij Srivastava <kshitij.srivastava@uber.com>
2020-08-28 11:48:10 -0400

    narrow converter added successfully

--- torch2trt/converters/narrow.py
+++ torch2trt/converters/narrow.py
@@ -2,32 +2,39 @@ import tensorrt as trt
 from torch2trt.torch2trt import *
 from torch2trt.module_test import add_module_test
 
-
+@tensorrt_converter('torch.Tensor.narrow')
 @tensorrt_converter('torch.narrow')
 def convert_narrow(ctx):
-    inputs = get_arg(ctx, 'input', pos=0, default=None) 
-    dim = get_arg(ctx, 'dim', pos=1, default=0) 
+    inputs = get_arg(ctx, 'input', pos=0, default=None)  
     start = get_arg(ctx, 'start', pos=2, default=None)
-    print("start",start, type(start))
-    input_trt= trt_(ctx.network, inputs)
-    
     output = ctx.method_return
-    output_shape = list(output.size())
-    #print(type(trt.Dims(start)),type(trt.Tensorrt.Dims(start)))
-    print(type(trt.tensorrt.Dims(output_shape)))
-    layer = ctx.network.add_slice(inputs=input_trt,shape=trt.tensorrt.Dims(output_shape))
+    shape = list(inputs.shape)
+    start = [0]*len(shape)
+    stride = [1]*len(shape)
+    dim = ctx.method_args[1] if get_arg(ctx, 'dim', pos=1, default=0) >=0 else len(shape)+get_arg(ctx, 'dim', pos=1, default=0)
+    start[dim] = ctx.method_args[2]
+    shape[dim] = ctx.method_args[3] 
+    # not consider batch dimension
+    input_trt = trt_(ctx.network,inputs)
+    layer = ctx.network.add_slice(input=input_trt,start=start[1:], shape=shape[1:],stride=stride[1:])
     output._trt = layer.get_output(0)
 
-class narrow(torch.nn.Module):
+class Narrow(torch.nn.Module):
     def __init__(self, dim, start, length):
-        super(Cat, self).__init__()
+        super(Narrow, self).__init__()
         self.dim = dim
         self.start = start
         self.length = length
 
-    def forward(self, *x):
-        return torch.narrow(x, dim=self.dim,start=self.start,length=self.length)
+    def forward(self, x):
+        return torch.narrow(x,self.dim,self.start,self.length)
 
-@add_module_test(torch.float32, torch.device('cuda'), [(1, 4, 4), (1, 3, 4), (1, 17, 4)])
-def test_narrow_basic():
-    return narrow(1,0,2)
+@add_module_test(torch.float32, torch.device('cuda'), [(1,3,224,224)])
+def test_narrow1():
+    return Narrow(1,0,2)
+
+@add_module_test(torch.float32, torch.device('cuda'), [(1,3,224,224)])
+def test_narrow2():
+    return Narrow(2,0,50)
+
+
