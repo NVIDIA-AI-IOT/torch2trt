120d585e989bfbe2c3dc48ebd41423a320ec3a2e
John Welsh <jwelsh@nvidia.com>
2020-09-03 00:10:05 +0000

    asdf

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 GITHUB=$1
 TAG=$2
 
 python3 scripts/dump_converters.py --github=$GITHUB --tag=$TAG > docs/converters.md
-=======
-TAG=$1
-
-# python3 scripts/dump_converters.py --tag=$TAG > docs/converters.md
-
-mike deploy $TAG
->>>>>>> 4933faf05cd9e5d9aef2b23e7fe692bfda5fbc06
