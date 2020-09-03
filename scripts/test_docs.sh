51609012ddf0a5e340dd73933a3b210d88afa424
John Welsh <jwelsh@nvidia.com>
2020-09-03 00:10:05 +0000

    asdf

--- scripts/test_docs.sh
+++ scripts/test_docs.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-TAG=$1
-
-<<<<<<< HEAD
 mkdocs serve --dev-addr=0.0.0.0:8000
-=======
-mike set-default $TAG
-mike serve --dev-addr=0.0.0.0:8000
->>>>>>> 4933faf05cd9e5d9aef2b23e7fe692bfda5fbc06
