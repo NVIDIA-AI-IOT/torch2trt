7f8d43d1eaf871d42abb8870062537293ebae65e
Kshitij Srivastava <kshitij.srivastava@uber.com>
2020-11-05 10:43:50 -0500

    Added inference layer for nvidia qat lib changed converter for quantconv

---
+++ examples/layers/quant_conv.py
@@ -0,0 +1,24 @@
+"""
+These layers represent the inference side for nvidia qat library
+"""
+
+import torch
+
+class QuantConv2d(torch.nn.Conv2d):
+    '''
+    mimicking inference side of things
+    '''
+    def __init__(self,in_channels,
+                out_channels,
+                kernel_size,
+                stride=1,
+                padding=0,
+                dilation=1,
+                groups=1,
+                bias=True,
+                padding_mode='zeros'):
+        super().__init__(in_channels,out_channels,kernel_size,stride=stride,padding=padding,dilation=dilatio,groups=groups,bias=bias,padding_mode=padding_mode)
+        self.register_buffer('_input_quantizer.learned_amax',torch.tensor(1))
+        self.register_buffer('_weight_quantizer.learned_amax',torch.tensor(1))
+
+
