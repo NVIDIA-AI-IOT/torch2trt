ce4b7fdd86e0b92820f59d70be0f4278f534e196
Peter Reutemann <fracpete@gmail.com>
2021-01-14 09:11:32 +1300

    torch only gets imported when --plugins flag present, allowing the wheel compilation inside a docker container (at build time, the docker container doesn't have access to the nvidia runtime and fails importing with 'SError: libcurand.so.10: cannot open shared object file: No such file or directory')

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 import sys
-import torch
 from setuptools import setup, find_packages
-from torch.utils.cpp_extension import BuildExtension, CUDAExtension, CppExtension
 
 def trt_inc_dir():
     return "/usr/include/aarch64-linux-gnu"
@@ -11,29 +9,39 @@ def trt_lib_dir():
 
 ext_modules = []
 
-plugins_ext_module = CUDAExtension(
-        name='plugins', 
-        sources=[
-            'torch2trt/plugins/plugins.cpp'
-        ],
-        include_dirs=[
-            trt_inc_dir()
-        ],
-        library_dirs=[
-            trt_lib_dir()
-        ],
-        libraries=[
-            'nvinfer'
-        ],
-        extra_compile_args={
-            'cxx': ['-DUSE_DEPRECATED_INTLIST'] if torch.__version__ < "1.5" else [],
-            'nvcc': []
-        }
-    )
 if '--plugins' in sys.argv:
+    import torch
+    from torch.utils.cpp_extension import CUDAExtension
+    plugins_ext_module = CUDAExtension(
+            name='plugins',
+            sources=[
+                'torch2trt/plugins/plugins.cpp'
+            ],
+            include_dirs=[
+                trt_inc_dir()
+            ],
+            library_dirs=[
+                trt_lib_dir()
+            ],
+            libraries=[
+                'nvinfer'
+            ],
+            extra_compile_args={
+                'cxx': ['-DUSE_DEPRECATED_INTLIST'] if torch.__version__ < "1.5" else [],
+                'nvcc': []
+            }
+        )
     ext_modules.append(plugins_ext_module)
     sys.argv.remove('--plugins')
-    
+
+
+def get_cmdclass():
+    if '--plugins' in sys.argv:
+        from torch.utils.cpp_extension import BuildExtension
+        return {'build_ext': BuildExtension}
+    else:
+        return {}
+
 
 setup(
     name='torch2trt',
@@ -42,5 +50,6 @@ setup(
     packages=find_packages(),
     ext_package='torch2trt',
     ext_modules=ext_modules,
-    cmdclass={'build_ext': BuildExtension}
+    cmdclass=get_cmdclass()
 )
+
