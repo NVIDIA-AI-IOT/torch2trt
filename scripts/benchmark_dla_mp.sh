3ea9a27daae91c4e003e4e8c1747ac16194c1bcb
John Welsh <jaybdub@users.noreply.github.com>
2021-03-01 14:27:17 -0800

    benchmark dla mp

--- scripts/benchmark_dla_mp.sh
+++ scripts/benchmark_dla_mp.sh
@@ -3,6 +3,9 @@
 # generates model cache for use with multiprocessing benchmark
 BATCH_SIZE=1
 
+# NOTE: currently we can't use DLA1 with deserialized engines.  The reason is likely because we need to set the DLA in runtime,
+# but TensorRT python API does not expose this feature from the TensorRT C++ API.
+
 # INT8
 
 # 2 - 0
@@ -17,3 +20,18 @@ python3 scripts/benchmark_dla.py --model=resnet18_int8_dla --batch_size=$BATCH_S
 # echo "Profiling 1GPU-2DLA"
 python3 scripts/benchmark_dla.py --model=resnet18_int8_dla --batch_size=$BATCH_SIZE --use_cache --dla_core=0 --distributed --rank=0 --world_size=2 &
 python3 scripts/benchmark_dla.py --model=resnet18_int8_dla --batch_size=$BATCH_SIZE --use_cache --dla_core=1 --distributed --rank=1 --world_size=2
+
+# FP16
+
+# 2 - 0
+python3 scripts/benchmark_dla.py --model=resnet18_fp16_gpu --batch_size=$BATCH_SIZE --use_cache --distributed --rank=0 --world_size=2 &
+python3 scripts/benchmark_dla.py --model=resnet18_fp16_gpu --batch_size=$BATCH_SIZE --use_cache --distributed --rank=1 --world_size=2
+
+# 1 - 1
+python3 scripts/benchmark_dla.py --model=resnet18_fp16_gpu --batch_size=$BATCH_SIZE --use_cache --distributed --rank=0 --world_size=2 &
+python3 scripts/benchmark_dla.py --model=resnet18_fp16_dla --batch_size=$BATCH_SIZE --use_cache --dla_core=0 --distributed --rank=1 --world_size=2 
+
+# 0 - 2
+# echo "Profiling 1GPU-2DLA"
+python3 scripts/benchmark_dla.py --model=resnet18_fp16_dla --batch_size=$BATCH_SIZE --use_cache --dla_core=0 --distributed --rank=0 --world_size=2 &
+python3 scripts/benchmark_dla.py --model=resnet18_fp16_dla --batch_size=$BATCH_SIZE --use_cache --dla_core=1 --distributed --rank=1 --world_size=2
