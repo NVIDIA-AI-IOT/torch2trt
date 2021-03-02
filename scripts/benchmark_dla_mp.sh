4bdb4f0235765c744aaf88621d4ae9e9290d14f1
John Welsh <jaybdub@users.noreply.github.com>
2021-03-02 12:25:20 -0800

    bnehcmark mp

--- scripts/benchmark_dla_mp.sh
+++ scripts/benchmark_dla_mp.sh
@@ -16,10 +16,9 @@ python3 scripts/benchmark_dla.py --model=resnet18_int8_gpu --batch_size=$BATCH_S
 python3 scripts/benchmark_dla.py --model=resnet18_int8_gpu --batch_size=$BATCH_SIZE --use_cache --distributed --rank=0 --world_size=2 &
 python3 scripts/benchmark_dla.py --model=resnet18_int8_dla --batch_size=$BATCH_SIZE --use_cache --dla_core=0 --distributed --rank=1 --world_size=2 
 
-# 0 - 2
-# echo "Profiling 1GPU-2DLA"
-python3 scripts/benchmark_dla.py --model=resnet18_int8_dla --batch_size=$BATCH_SIZE --use_cache --dla_core=0 --distributed --rank=0 --world_size=2 &
-python3 scripts/benchmark_dla.py --model=resnet18_int8_dla --batch_size=$BATCH_SIZE --use_cache --dla_core=1 --distributed --rank=1 --world_size=2
+# 0 - 2 (skip use-cache because of missing setDLACore when deserializing engine)
+python3 scripts/benchmark_dla.py --model=resnet18_int8_dla --batch_size=$BATCH_SIZE --dla_core=0 --distributed --rank=0 --world_size=2 &
+python3 scripts/benchmark_dla.py --model=resnet18_int8_dla --batch_size=$BATCH_SIZE --dla_core=1 --distributed --rank=1 --world_size=2
 
 # FP16
 
@@ -32,6 +31,5 @@ python3 scripts/benchmark_dla.py --model=resnet18_fp16_gpu --batch_size=$BATCH_S
 python3 scripts/benchmark_dla.py --model=resnet18_fp16_dla --batch_size=$BATCH_SIZE --use_cache --dla_core=0 --distributed --rank=1 --world_size=2 
 
 # 0 - 2
-# echo "Profiling 1GPU-2DLA"
-python3 scripts/benchmark_dla.py --model=resnet18_fp16_dla --batch_size=$BATCH_SIZE --use_cache --dla_core=0 --distributed --rank=0 --world_size=2 &
-python3 scripts/benchmark_dla.py --model=resnet18_fp16_dla --batch_size=$BATCH_SIZE --use_cache --dla_core=1 --distributed --rank=1 --world_size=2
+python3 scripts/benchmark_dla.py --model=resnet18_fp16_dla --batch_size=$BATCH_SIZE --dla_core=0 --distributed --rank=0 --world_size=2 &
+python3 scripts/benchmark_dla.py --model=resnet18_fp16_dla --batch_size=$BATCH_SIZE --dla_core=1 --distributed --rank=1 --world_size=2
