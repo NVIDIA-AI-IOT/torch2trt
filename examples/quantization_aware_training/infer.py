f99137e23f2388ba8a851eccbe32fbb08ced86ee
Kshitij Srivastava <kshitij.srivastava@uber.com>
2021-03-09 11:40:30 -0500

    refined examples

--- examples/quantization_aware_training/infer.py
+++ examples/quantization_aware_training/infer.py
@@ -7,8 +7,7 @@ import torchvision
 import argparse
 import os,sys 
 from datasets.cifar10 import Cifar10Loaders
-from models.models import vanilla_cnn
-from utils.utilities import calculate_accuracy, timeGraph,printStats 
+from utils.utilities import calculate_accuracy, timeGraph,printStats,mapping_names_resnets 
 from models.resnet import resnet18,resnet34
 from parser import parse_args
 from torch2trt import torch2trt
@@ -38,16 +37,6 @@ def main():
             model=resnet34(qat_mode=True,infer=True)
         else:
             model=resnet34()
-    elif args.m == "vanilla_cnn2":
-        if args.netqat:
-            model =vanilla_cnn2(qat_mode=True,infer=True)
-        else:
-            model=vanilla_cnn2()
-    elif args.m == "vanilla_cnn":
-        if args.netqat:
-            model=vanilla_cnn(qat_mode=True,infer=True)
-        else:
-            model=vanilla_cnn()
     else:
         raise NotImplementedError("{} model not found".format(args.m))
 
@@ -56,41 +45,37 @@ def main():
 
     if args.load_ckpt:
         checkpoint = torch.load(args.load_ckpt)
+        if not args.netqat:
+            checkpoint = mapping_names_resnets(checkpoint)
         model.load_state_dict(checkpoint['model_state_dict'],strict=True)
         print("===>>> Checkpoint loaded successfully from {} ".format(args.load_ckpt))
     
     test_accuracy = calculate_accuracy(model,test_loader)
-    print(" Test accuracy: {0} ".format(test_accuracy))
+    print(" Test accuracy for Pytorch model: {0} ".format(test_accuracy))
     rand_in = torch.randn([128,3,32,32],dtype=torch.float32).cuda()
     
     #Converting the model to TRT
-    trt_model_fp16 = torch2trt(model,[rand_in],log_level=trt.Logger.INFO,fp16_mode=True,max_batch_size=128)
-    test_accuracy = calculate_accuracy(trt_model_fp16,test_loader)
-    print(" TRT test accuracy: {0}".format(test_accuracy))
-    timings = timeGraph(trt_model_fp16, rand_in, args.iter)
-    printStats('fp16 trt model', timings, args.b)
+    if args.FP16:
+        trt_model_fp16 = torch2trt(model,[rand_in],log_level=trt.Logger.INFO,fp16_mode=True,max_batch_size=128)
+        test_accuracy = calculate_accuracy(trt_model_fp16,test_loader)
+        print(" TRT test accuracy at FP16: {0}".format(test_accuracy))
     
-
-    trt_model_int8 = torch2trt(model,[rand_in],log_level=trt.Logger.INFO,fp16_mode=True,int8_mode=True,max_batch_size=128,qat_mode=True)
-    test_accuracy = calculate_accuracy(trt_model_int8,test_loader)
-    print(" TRT test accuracy: {0}".format(test_accuracy))
-    timings = timeGraph(trt_model_int8, rand_in, args.iter)
-    printStats('int8 trt model', timings, args.b)
-
-    del trt_model_int8
+    if args.INT8QAT:
+        trt_model_int8 = torch2trt(model,[rand_in],log_level=trt.Logger.INFO,fp16_mode=True,int8_mode=True,max_batch_size=128,qat_mode=True)
+        test_accuracy = calculate_accuracy(trt_model_int8,test_loader)
+        print(" TRT test accuracy at INT8 QAT: {0}".format(test_accuracy))
     
-    ##preparing calib dataset
-    calib_dataset = list()
-    for i, sam in enumerate(test_loader):
-        calib_dataset.extend(sam[0])
-        if i ==5:
-            break
+    if args.INT8PTC:
+        ##preparing calib dataset
+        calib_dataset = list()
+        for i, sam in enumerate(test_loader):
+            calib_dataset.extend(sam[0])
+            if i ==5:
+                break
 
-    trt_model_calib_int8 = torch2trt(model,[rand_in],log_level=trt.Logger.INFO,fp16_mode=True,int8_calib_dataset=calib_dataset,int8_mode=True,max_batch_size=128)
-    test_accuracy = calculate_accuracy(trt_model_calib_int8,test_loader)
-    print(" TRT test accuracy: {0}".format(test_accuracy))
-
-    del trt_model_calib_int8
+        trt_model_calib_int8 = torch2trt(model,[rand_in],log_level=trt.Logger.INFO,fp16_mode=True,int8_calib_dataset=calib_dataset,int8_mode=True,max_batch_size=128)
+        test_accuracy = calculate_accuracy(trt_model_calib_int8,test_loader)
+        print(" TRT test accuracy at INT8 PTC: {0}".format(test_accuracy))
 
 if __name__ == "__main__":
     main()
--- examples/quantization_aware_training/train.py
+++ examples/quantization_aware_training/train.py
@@ -8,10 +8,12 @@ import torch.optim as optim
 from datasets.cifar10 import Cifar10Loaders
 from models.models import vanilla_cnn
 from models.resnet import resnet18 , resnet34
-from utils.utilities import calculate_accuracy , add_missing_keys, transfer_learning_resnet18,transfer_learning_resnet34, mapping_names 
+from utils.utilities import calculate_accuracy , add_missing_keys, transfer_learning_resnet18,transfer_learning_resnet34, mapping_names,mapping_names_resnets 
 from parser import parse_args
 import time
 from torch.optim.lr_scheduler import StepLR
+from torch2trt import torch2trt
+import tensorrt as trt 
 
 def main():
     args = parse_args()
@@ -54,11 +56,6 @@ def main():
             model=resnet34(qat_mode=True)
         else:
             model=resnet34()
-    elif args.m == "vanilla_cnn":
-        if args.netqat:
-            model=vanilla_cnn(qat_mode=True)
-        else:
-            model=vanilla_cnn()
     elif args.m == 'resnet34-tl':
         model = transfer_learning_resnet34()
     elif args.m == "resnet18-tl": ## resnet18 transfer learning
@@ -133,9 +130,9 @@ def main():
 
         print("Epoch: {0} | Loss: {1} | Test accuracy: {2}| Time Taken (sec): {3} ".format(epoch+1, np.around(running_loss,6), test_accuracy, np.around((end-start),4)))
 
-        best_ckpt_filename = dir_name + "/ckpt_" + str(epoch)
         ##Save the best checkpoint
         if test_accuracy > best_test_accuracy:
+            best_ckpt_filename = dir_name + "/ckpt_" + str(epoch)
             best_test_accuracy = test_accuracy
             torch.save({
                 'epoch': epoch,
@@ -144,6 +141,43 @@ def main():
                 'loss': running_loss,
                 }, best_ckpt_filename)
     print("Training finished")
+    
+    ## Running metrics
+    if args.test_trt:
+        if args.m == 'resnet34-tl' or args.m == 'resnet34':
+            model = transfer_learning_resnet34(pretrained=False)
+        elif args.m == 'resnet18-tl' or args.m == 'resnet18':
+            model= transfer_learning_resnet18(pretrained=False)
+        else:
+            raise NotImplementedError("model {} is not defined".format(args.m))
+        
+        model=model.cuda().eval()
+        checkpoint = torch.load(best_ckpt_filename)
+        model.load_state_dict(checkpoint['model_state_dict'],strict=True)
+        
+        pytorch_test_accuracy = calculate_accuracy(model,test_loader)
+        rand_in = torch.randn([128,3,32,32],dtype=torch.float32).cuda()
+
+        if args.FP16:
+            trt_model_fp16 = torch2trt(model,[rand_in],log_level=trt.Logger.INFO,fp16_mode=True,max_batch_size=128)
+            trtfp16_test_accuracy = calculate_accuracy(trt_model_fp16,test_loader)
+    
+        if args.INT8PTC:
+            ##preparing calib dataset
+            calib_dataset = list()
+            for i, sam in enumerate(test_loader):
+                calib_dataset.extend(sam[0])
+                if i ==5:
+                    break
+
+            trt_model_calib_int8 = torch2trt(model,[rand_in],log_level=trt.Logger.INFO,fp16_mode=True,int8_calib_dataset=calib_dataset,int8_mode=True,max_batch_size=128)
+            int8_test_accuracy = calculate_accuracy(trt_model_calib_int8,test_loader)
+
+        print("Test Accuracy")
+        print("Pytorch model :",pytorch_test_accuracy)
+        print("TRT FP16 model :",trtfp16_test_accuracy)
+        print("TRT INT8 PTC model :",int8_test_accuracy)
+
 
 if __name__ == "__main__":
     main()
