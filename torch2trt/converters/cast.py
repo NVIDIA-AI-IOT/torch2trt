23e1b89c81be8cd6bf96de25d66027164dc38a2f
jaylee <ljay189@gmail.com>
2021-08-11 19:13:52 +0900

    Removed redundant tests and converters.

--- torch2trt/converters/cast.py
+++ torch2trt/converters/cast.py
@@ -15,22 +15,20 @@ def convert_cast(ctx):
     output._trt = layer.get_output(0)
 
 
-@tensorrt_converter("torch.float")
 @tensorrt_converter("torch.Tensor.float")
 def convert_float(ctx):
     convert_cast(ctx)
 
 
-@tensorrt_converter("torch.bool")
 @tensorrt_converter("torch.Tensor.bool")
 def convert_bool(ctx):
     convert_cast(ctx)
 
 
-@tensorrt_converter("torch.float")
 @tensorrt_converter("torch.Tensor.float")
 def convert_bool(ctx):
     convert_cast(ctx)
+
 
 # Used for torch.Tensor.<cast> tests
 # --------------------------------------------
@@ -75,48 +73,3 @@ def test_torch_int_cast():
 @add_module_test(torch.int32, torch.device('cuda'), [(1, 3, 3)])
 def test_torch_bool_casting():
     return TorchBool()
-
-
-# Used for torch.<cast> tests
-# --------------------------------------------
-
-class DotFloat(torch.nn.Module):
-    def __init__(self):
-        super(DotFloat, self).__init__()
-
-    def forward(self, x):
-        return x.float()
-
-
-class DotInt(torch.nn.Module):
-    def __init__(self):
-        super(DotInt, self).__init__()
-
-    def forward(self, x):
-        return x.int()
-
-
-class DotBool(torch.nn.Module):
-    def __init__(self):
-        super(DotBool, self).__init__()
-
-    def forward(self, x):
-        return x.bool()
-
-
-@add_module_test(torch.bool, torch.device('cuda'), [(1, 3, 3)])
-@add_module_test(torch.int32, torch.device('cuda'), [(1, 3, 3)])
-def test_float_cast():
-    return DotFloat()
-
-
-@add_module_test(torch.float32, torch.device('cuda'), [(1, 3, 3)])
-@add_module_test(torch.int32, torch.device('cuda'), [(1, 3, 3)])
-def test_int_cast():
-    return DotInt()
-
-
-@add_module_test(torch.float32, torch.device('cuda'), [(1, 3, 3)])
-@add_module_test(torch.int32, torch.device('cuda'), [(1, 3, 3)])
-def test_bool_cast():
-    return DotBool()
