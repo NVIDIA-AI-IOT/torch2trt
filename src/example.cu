af667a16d9135343cb2f6b7a9c12b5818c5b645c
John Welsh <jaybdub@users.noreply.github.com>
2022-01-24 21:53:01 +0000

    plugin inst

--- src/example.cu
+++ src/example.cu
@@ -1,19 +1,110 @@
 #include "example.h"
+#include "cuda_runtime.h"
+
+
+
+namespace torch2trt_plugins {
 
 
 template<typename T>
-__global__ void cuda_double_kernel(T *x) {
-    *x = (*x) * 2;
+__global__ void exampleKernel(T *x, int size) {
+    int index = blockDim.x * blockIdx.x + threadIdx.x;
+    if (index < size) {
+        x[index] *= 2;
+    }
 }
 
-template __global__ void cuda_double_kernel<float>(float *);
-template __global__ void cuda_double_kernel<int>(int *);
+template __global__ void exampleKernel<float>(float *, int);
+template __global__ void exampleKernel<int>(int *, int);
 
 
 template<typename T>
-void cuda_double(T *x) {
-    cuda_double_kernel<<<1, 1>>>(x);
+void exampleFuncton(T *x, int size) {
+    int nThreads = 32;
+    int nBlocks = (size / 32) + 1;
+    exampleKernel<<<nBlocks, nThreads>>>(x, size);
 }
 
-template void cuda_double<float>(float *);
-template void cuda_double<int>(int *);
+template void exampleFuncton<float>(float *, int);
+template void exampleFuncton<int>(int *, int);
+
+
+ExamplePlugin::ExamplePlugin() {
+
+}
+
+ExamplePlugin::~ExamplePlugin() {
+
+}
+
+AsciiChar const * ExamplePlugin::getPluginType() const noexcept {
+    return "ExamplePlugin";
+}
+
+AsciiChar const * ExamplePlugin::getPluginVersion() const noexcept {
+    return "1";
+}
+
+int32_t ExamplePlugin::getNbOutputs() const noexcept {
+    return 1;
+}
+
+Dims ExamplePlugin::getOutputDimensions(int32_t index, Dims const* inputs, int32_t nbInputDims) noexcept {
+    return inputs[0];
+}
+
+bool ExamplePlugin::supportsFormat(DataType type, PluginFormat format) const noexcept {
+    return (type == DataType::kFLOAT) || (type == DataType::kINT32);
+}
+
+void ExamplePlugin::configureWithFormat(Dims const* inputDims, int32_t nbInputs, Dims const* outputDims, int32_t nbOutputs,
+    DataType type, PluginFormat format, int32_t maxBatchSize) noexcept {
+    Dims d = inputDims[0];
+    this->inputSize = 1;
+    for (int i = 0; i < d.nbDims; i++) {
+        this->inputSize *= d.d[i];
+    }
+};
+
+int32_t ExamplePlugin::initialize() noexcept {
+    return 0;
+};
+
+void ExamplePlugin::terminate() noexcept {
+
+};
+
+size_t ExamplePlugin::getWorkspaceSize(int32_t maxBatchSize) const noexcept {
+    return 0;
+};
+
+int32_t ExamplePlugin::enqueue(int32_t batchSize, void const* const* inputs, void* const* outputs, void* workspace,
+    cudaStream_t stream) noexcept {
+    return 0;
+};
+
+size_t ExamplePlugin::getSerializationSize() const noexcept {
+    return 0;
+};
+
+void ExamplePlugin::serialize(void* buffer) const noexcept {
+
+};
+
+void ExamplePlugin::destroy() noexcept {
+
+};
+
+IPluginV2* ExamplePlugin::clone() const noexcept { 
+    return nullptr; 
+};
+
+void ExamplePlugin::setPluginNamespace(AsciiChar const* pluginNamespace) noexcept {
+
+};
+
+AsciiChar const* ExamplePlugin::getPluginNamespace() const noexcept {
+    return "";
+};
+
+}
--- src/example_test.cu
+++ src/example_test.cu
@@ -1,6 +1,8 @@
 #include <catch2/catch_all.hpp>
-#include "cuda_runtime.h"
 #include "example.h"
+
+
+using namespace torch2trt_plugins;
 
 
 TEMPLATE_TEST_CASE("Example cuda test", "[example][template]" , int) {
@@ -10,7 +12,23 @@ TEMPLATE_TEST_CASE("Example cuda test", "[example][template]" , int) {
     cudaMalloc(&x_gpu, sizeof(int));
     x_cpu = 2;
     cudaMemcpy(x_gpu, &x_cpu, sizeof(int), cudaMemcpyHostToDevice);
-    cuda_double<TestType>(x_gpu);
+    exampleFuncton<TestType>(x_gpu, 1);
     cudaMemcpy(&x_cpu, x_gpu, sizeof(int), cudaMemcpyDeviceToHost);
     REQUIRE(x_cpu == 4);
+}
+
+
+TEST_CASE("Example plugin test", "[example]") {
+    auto plugin = ExamplePlugin();
+    Dims3 inputDims(3, 4, 5);
+    plugin.configureWithFormat(
+        &inputDims,
+        1,
+        &inputDims,
+        1,
+        DataType::kINT32,
+        PluginFormat::kCHW4,
+        1
+    );
+    REQUIRE(plugin.inputSize == 3 * 4 * 5);
 }
