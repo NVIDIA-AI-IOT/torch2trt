18cf5503dbac696060fc8a70d9517c56d67854f8
John Welsh <jaybdub@users.noreply.github.com>
2022-01-24 22:25:22 +0000

    plugin enqueu

--- src/example.cu
+++ src/example.cu
@@ -7,27 +7,26 @@ namespace torch2trt_plugins {
 
 
 template<typename T>
-__global__ void exampleKernel(T *x, int size) {
+__global__ void exampleKernel(T *x, T *y, int size) {
     int index = blockDim.x * blockIdx.x + threadIdx.x;
     if (index < size) {
-        x[index] *= 2;
+        y[index] = x[index] * 2;
     }
 }
 
-template __global__ void exampleKernel<float>(float *, int);
-template __global__ void exampleKernel<int>(int *, int);
+template __global__ void exampleKernel<float>(float *, float *, int);
+template __global__ void exampleKernel<int>(int *, int *, int);
 
 
 template<typename T>
-void exampleFuncton(T *x, int size) {
+void exampleFuncton(T *x, T *y, int size, cudaStream_t stream) {
     int nThreads = 32;
     int nBlocks = (size / 32) + 1;
-    exampleKernel<<<nBlocks, nThreads>>>(x, size);
+    exampleKernel<<<nBlocks, nThreads, 0, stream>>>(x, y, size);
 }
 
-template void exampleFuncton<float>(float *, int);
-template void exampleFuncton<int>(int *, int);
-
+template void exampleFuncton<float>(float *, float *, int, cudaStream_t);
+template void exampleFuncton<int>(int *, int *, int, cudaStream_t);
 
 ExamplePlugin::ExamplePlugin() {
 
@@ -64,6 +63,7 @@ void ExamplePlugin::configureWithFormat(Dims const* inputDims, int32_t nbInputs,
     for (int i = 0; i < d.nbDims; i++) {
         this->inputSize *= d.d[i];
     }
+    this->dataType = type;
 };
 
 int32_t ExamplePlugin::initialize() noexcept {
@@ -71,7 +71,7 @@ int32_t ExamplePlugin::initialize() noexcept {
 };
 
 void ExamplePlugin::terminate() noexcept {
-
+    
 };
 
 size_t ExamplePlugin::getWorkspaceSize(int32_t maxBatchSize) const noexcept {
@@ -80,6 +80,16 @@ size_t ExamplePlugin::getWorkspaceSize(int32_t maxBatchSize) const noexcept {
 
 int32_t ExamplePlugin::enqueue(int32_t batchSize, void const* const* inputs, void* const* outputs, void* workspace,
     cudaStream_t stream) noexcept {
+    const int totalSize = batchSize * this->inputSize;
+    switch (this->dataType) {
+        case DataType::kFLOAT: {
+            exampleFuncton<float>((float*) inputs[0], (float*) outputs[0], totalSize, stream);
+            break;
+        }
+        case DataType::kINT32: {
+            exampleFuncton<int>((int*) inputs[0], (int*) outputs[0], totalSize, stream);
+        }
+    }
     return 0;
 };
 
--- src/example_test.cu
+++ src/example_test.cu
@@ -5,16 +5,19 @@
 using namespace torch2trt_plugins;
 
 
-TEMPLATE_TEST_CASE("Example cuda test", "[example][template]" , int) {
-    TestType x_cpu;
+TEMPLATE_TEST_CASE("Example cuda test", "[example][template]" , int, float) {
+    TestType *x_cpu;
     TestType *x_gpu;
     
-    cudaMalloc(&x_gpu, sizeof(int));
-    x_cpu = 2;
-    cudaMemcpy(x_gpu, &x_cpu, sizeof(int), cudaMemcpyHostToDevice);
-    exampleFuncton<TestType>(x_gpu, 1);
-    cudaMemcpy(&x_cpu, x_gpu, sizeof(int), cudaMemcpyDeviceToHost);
-    REQUIRE(x_cpu == 4);
+    x_cpu = (TestType *) malloc(sizeof(TestType));
+    cudaMalloc(&x_gpu, sizeof(TestType));
+    *x_cpu = 2;
+    cudaMemcpy(x_gpu, x_cpu, sizeof(TestType), cudaMemcpyHostToDevice);
+    exampleFuncton<TestType>(x_gpu, x_gpu, 1);
+    cudaMemcpy(x_cpu, x_gpu, sizeof(TestType), cudaMemcpyDeviceToHost);
+    REQUIRE(*x_cpu == 4);
+    cudaFree(x_gpu);
+    free(x_cpu);
 }
 
 
@@ -31,4 +34,48 @@ TEST_CASE("Example plugin test", "[example]") {
         1
     );
     REQUIRE(plugin.inputSize == 3 * 4 * 5);
+}
+
+TEMPLATE_TEST_CASE("Example plugin enqueue test", "[example][template]" , int, float) {
+    TestType *x_cpu;
+    TestType *x_gpu;
+
+    // create and configure plugin
+    auto plugin = ExamplePlugin();
+    Dims3 inputDims(3, 4, 5);
+    plugin.configureWithFormat(
+        &inputDims,
+        1,
+        &inputDims,
+        1,
+        DataType::kINT32,
+        PluginFormat::kCHW4,
+        1
+    );
+
+    // get sizes
+    int count = 3 * 4 * 5;
+    int size = count * sizeof(TestType);
+    
+    // allocate buffers
+    x_cpu = (TestType *) malloc(size);
+    cudaMalloc(&x_gpu, size);
+
+    // populate host
+    for (int i = 0; i < count; i++) {
+        x_cpu[i] = 2;
+    }
+    
+    // copy to device
+    cudaMemcpy(x_gpu, x_cpu, size, cudaMemcpyHostToDevice);
+
+    // execute plugin
+    plugin.enqueue(1, (void**) &x_gpu, (void**) &x_gpu, (void*) nullptr, 0);
+
+    // copy to host
+    cudaMemcpy(x_cpu, x_gpu, size, cudaMemcpyDeviceToHost);
+
+    for (int i = 0; i < count; i++) {
+        x_cpu[i] = 4;
+    }
 }
