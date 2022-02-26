2d8f03013483abe607bc52fe5598d077e2a96de4
Ilyabasharov <ilya.basharov.98@mail.ru>
2022-02-26 20:42:19 +0300

    add avg_pool1d converter

--- torch2trt/converters/avg_pool.py
+++ torch2trt/converters/avg_pool.py
@@ -2,6 +2,55 @@ from torch2trt.torch2trt import *
 from torch2trt.module_test import add_module_test
 
 
+@tensorrt_converter('torch.nn.functional.avg_pool1d')
+def convert_avg_pool1d(ctx):
+    # At the time of this implementation, TensorRT 8.x does not yet support avg pooling in 1D using `add_pooling_nd(...)`.
+    # As such, we use a workaround here, by unsqueezing another dimension into the input (thus transforming it from
+    # (N, C, L) to (N, C, L, 1)) so that we can use 2D max pooling across the last three dimensions.
+
+    input = get_arg(ctx, 'input', pos=0, default=None)
+    input_trt = trt_(ctx.network, input)
+    output = ctx.method_return
+    
+    kernel_size = get_arg(ctx, 'kernel_size', pos=1, default=None)
+    stride = get_arg(ctx, 'stride', pos=2, default=None)
+    padding = get_arg(ctx, 'padding', pos=3, default=0)
+    ceil_mode = get_arg(ctx, 'ceil_mode', pos=4, default=False)
+    count_include_pad = get_arg(ctx, 'count_include_pad', pos=5, default=True)
+
+    # Convert inputs to be 2d compatible as inputs will always be 1d.
+    kernel_size = (kernel_size, 1)
+    stride = kernel_size if not stride else (stride, 1)
+    padding = (padding, 0)
+
+    # Shuffle layer to unsqueeze another dimension for 2D max pooling.
+    unsqueeze_layer = ctx.network.add_shuffle(input_trt)
+    set_layer_precision(ctx, unsqueeze_layer)
+    unsqueeze_layer.reshape_dims = tuple([*input_trt.shape, 1])
+    unsqueeze_trt = unsqueeze_layer.get_output(0)
+
+    # Use 2D max pooling here to fake 1D max pooling.
+    layer = ctx.network.add_pooling_nd(
+        input=unsqueeze_trt,
+        type=trt.PoolingType.AVERAGE,
+        window_size=kernel_size,
+    )
+    set_layer_precision(ctx, layer)
+    layer.stride_nd = stride
+    layer.padding_nd = padding
+    layer.average_count_excludes_padding = not count_include_pad
+
+    if ceil_mode:
+        layer.padding_mode = trt.PaddingMode.EXPLICIT_ROUND_UP
+
+    pooling_trt = layer.get_output(0)
+
+    # Shuffle layer to squeeze out dimension that was just added for 2D max pooling so return is still in 1D.
+    squeeze_layer = ctx.network.add_shuffle(pooling_trt)
+    set_layer_precision(ctx, squeeze_layer)
+    squeeze_layer.reshape_dims = tuple(pooling_trt.shape[:-1])
+    output._trt = squeeze_layer.get_output(0)
+
 @tensorrt_converter("torch.nn.functional.avg_pool2d", enabled=trt_version() < '7.0')
 def convert_avg_pool2d(ctx):
     # parse args
@@ -83,12 +132,14 @@ def convert_avg_pool_trt7(ctx):
         layer.padding_mode = trt.PaddingMode.EXPLICIT_ROUND_UP
 
     output._trt = layer.get_output(0)
- 
-
+    
+    
 @add_module_test(torch.float32, torch.device("cuda"), [(1, 3, 4, 6)])
 @add_module_test(torch.float32, torch.device("cuda"), [(1, 3, 5, 7)])
 def test_avg_pool2d_without_ceil_mode():
-    return torch.nn.AvgPool2d(kernel_size=3, stride=2, padding=1, ceil_mode=False)
+    return torch.nn.AvgPool2d(
+        kernel_size=3, stride=2, padding=1, ceil_mode=False
+    )
 
 
 @add_module_test(torch.float32, torch.device("cuda"), [(1, 3, 4, 6)])
@@ -102,10 +153,14 @@ def test_avg_pool2d_with_ceil_mode():
 @add_module_test(torch.float32, torch.device('cuda'), [(1, 3, 4, 4, 6)], enabled=trt_version() >= '7.0')
 @add_module_test(torch.float32, torch.device('cuda'), [(1, 3, 3, 5, 7)], enabled=trt_version() >= '7.0')
 def test_avg_pool3d_without_ceil_mode_trt7():
-    return torch.nn.AvgPool3d(kernel_size=3, stride=2, padding=1, ceil_mode=False)
+    return torch.nn.AvgPool3d(
+        kernel_size=3, stride=2, padding=1, ceil_mode=False
+    )
 
 
 @add_module_test(torch.float32, torch.device('cuda'), [(1, 3, 4, 4, 6)], enabled=trt_version() >= '7.0')
 @add_module_test(torch.float32, torch.device('cuda'), [(1, 3, 3, 5, 7)], enabled=trt_version() >= '7.0')
 def test_avg_pool3d_with_ceil_mode_trt7():
-    return torch.nn.AvgPool3d(kernel_size=3, stride=2, padding=1, ceil_mode=True, count_include_pad=False) # TRT does not support ceil_mode=True && count_include_pad=True
+    return torch.nn.AvgPool3d(
+        kernel_size=3, stride=2, padding=1, ceil_mode=True, count_include_pad=False
+    ) # TRT does not support ceil_mode=True && count_include_pad=True
