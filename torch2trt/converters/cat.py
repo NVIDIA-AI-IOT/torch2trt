cdb928eef10c37c78d5c55950712e14ae043b050
John Welsh <jaybdub@users.noreply.github.com>
2022-04-20 21:50:48 +0000

    cat shape fix

--- torch2trt/converters/cat.py
+++ torch2trt/converters/cat.py
@@ -13,7 +13,7 @@ def convert_cat(ctx):
 
     output = ctx.method_return
     trt_inputs = add_missing_trt_tensors(ctx.network, inputs)
-    trt_inputs = broadcast_trt_tensors(ctx.network, trt_inputs, len(output.shape) - 1)
+    trt_inputs = broadcast_trt_tensors(ctx.network, trt_inputs, len(output.shape))
 
     layer = ctx.network.add_concatenation(inputs=trt_inputs)
     layer.axis = dim
