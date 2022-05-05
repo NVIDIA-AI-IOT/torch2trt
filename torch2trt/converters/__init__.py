15c69e98b490831f489626a88b56b925b51ce0f9
John Welsh <jaybdub@users.noreply.github.com>
2022-05-05 23:38:03 +0000

    removed isolate plugins for python

--- torch2trt/converters/__init__.py
+++ torch2trt/converters/__init__.py
@@ -32,6 +32,7 @@ from .compare import *
 from .div import *
 from .einsum import *
 from .expand import *
+from .example_plugin import *
 from .floordiv import *
 from .gelu import *
 from .getitem import *
@@ -56,6 +57,7 @@ from .permute import *
 from .pow import *
 from .prelu import *
 from .prod import *
+from .reflection_pad_2d import *
 from .relu import *
 from .relu6 import *
 from .roll import *
