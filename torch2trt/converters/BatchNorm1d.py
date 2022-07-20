b3a213cf23d8fa194831d4722471fb72cd0dba85
John Welsh <jaybdub@users.noreply.github.com>
2022-07-20 15:54:42 -0700

    fix batch norm 1d for dynamic shapes

--- torch2trt/converters/BatchNorm1d.py
+++ torch2trt/converters/BatchNorm1d.py
@@ -3,7 +3,7 @@ from torch2trt.module_test import add_module_test
 
 
 @tensorrt_converter('torch.nn.BatchNorm1d.forward')
-def convert_BatchNorm2d(ctx):
+def convert_BatchNorm1d(ctx):
     module = ctx.method_args[0]
     input = ctx.method_args[1]
     input_trt = add_missing_trt_tensors(ctx.network, [input])[0]
@@ -17,15 +17,18 @@ def convert_BatchNorm2d(ctx):
     layer = ctx.network.add_shuffle(input_trt)
     
     if len(input.shape) == 2:
-        layer.reshape_dims = (input.shape[0], input.shape[1], 1, 1)
+        layer.reshape_dims = (0, 0, 1, 1)
     else:
-        layer.reshape_dims = (input.shape[0], input.shape[1], input.shape[2], 1)
+        layer.reshape_dims = (0, 0, 0, 1)
     
     layer = ctx.network.add_scale(layer.get_output(0), trt.ScaleMode.CHANNEL, bias, scale, power)
 
     # reshape back to 1D
     layer = ctx.network.add_shuffle(layer.get_output(0))
-    layer.reshape_dims = tuple(output.shape)
+    if len(input.shape) == 2:
+        layer.reshape_dims = (0, 0)
+    else:
+        layer.reshape_dims = (0, 0, 0)
     
     output._trt = layer.get_output(0)
 
