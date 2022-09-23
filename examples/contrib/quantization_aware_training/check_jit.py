e0742c19f1877806b0568f7cfe53e41b0076fef4
Kshitij Srivastava <kshitij@udel.edu>
2022-09-23 11:21:46 -0400

    added min reproducible file

---
+++ examples/contrib/quantization_aware_training/check_jit.py
@@ -0,0 +1,11 @@
+import torch
+from models.resnet import resnet18
+
+model=resnet18(qat_mode=True)
+
+rand_in = torch.randn([128,3,32,32],dtype=torch.float32).cuda()
+model = model.cuda().train()
+
+out = model(rand_in)
+model = torch.jit.script(model.eval())
+
