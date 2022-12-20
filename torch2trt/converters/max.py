978614e23afd89a56d05fd786f65e55e4b2bb987
Chao Zhang <chaoz@aurora.tech>
2022-12-19 22:46:52 -0500

    Support max/min on all dims

--- torch2trt/converters/max.py
+++ torch2trt/converters/max.py
@@ -15,7 +15,7 @@ def __convert_max_elementwise(ctx):
 
 def __convert_max_reduce(ctx):
     input = ctx.method_args[0]
-    dim = get_arg(ctx, 'dim', pos=1, default=tuple(range(1, len(input.shape))))
+    dim = get_arg(ctx, 'dim', pos=1, default=tuple(range(0, len(input.shape))))
     keepdim = get_arg(ctx, 'keepdim', pos=2, default=False)
     input_trt = add_missing_trt_tensors(ctx.network, [input])[0]
     if isinstance(ctx.method_return, torch.Tensor):
@@ -38,8 +38,10 @@ def convert_max(ctx):
 
 @add_module_test(torch.float32, torch.device('cuda'), [(1, 3)])
 @add_module_test(torch.float32, torch.device('cuda'), [(1, 3, 3)])
+@add_module_test(torch.float32, torch.device('cuda'), [(3, 3, 3)])
 def test_max():
-    return UnaryModule(lambda x: torch.max(x))
+    # Can't exit the network with a 0D tensor so we unsqueeze a dim.
+    return UnaryModule(lambda x: torch.max(x).unsqueeze(0))
 
 
 @add_module_test(torch.float32, torch.device('cuda'), [(1, 3)])
--- torch2trt/converters/min.py
+++ torch2trt/converters/min.py
@@ -15,7 +15,7 @@ def __convert_min_elementwise(ctx):
 
 def __convert_min_reduce(ctx):
     input = ctx.method_args[0]
-    dim = get_arg(ctx, 'dim', pos=1, default=tuple(range(1,len(input.shape))))
+    dim = get_arg(ctx, 'dim', pos=1, default=tuple(range(0, len(input.shape))))
     keepdim = get_arg(ctx, 'keepdim', pos=2, default=False)
     input_trt = add_missing_trt_tensors(ctx.network, [input])[0]
     if isinstance(ctx.method_return, torch.Tensor):
@@ -38,8 +38,10 @@ def convert_min(ctx):
 
 @add_module_test(torch.float32, torch.device('cuda'), [(1, 3)])
 @add_module_test(torch.float32, torch.device('cuda'), [(1, 3, 3)])
+@add_module_test(torch.float32, torch.device('cuda'), [(3, 3, 3)])
 def test_min():
-    return UnaryModule(lambda x: torch.min(x))
+    # Can't exit the network with a 0D tensor so we unsqueeze a dim.
+    return UnaryModule(lambda x: torch.min(x).unsqueeze(0))
 
 
 @add_module_test(torch.float32, torch.device('cuda'), [(1, 3)])
