4e002327aef2e22a0fc45e6543cccc913585bc57
Chao Zhang <chaoz@aurora.tech>
2023-02-03 17:28:59 -0500

    [converters/compare.py] Fix issue #838 comparing tensors to scalars

--- torch2trt/converters/compare.py
+++ torch2trt/converters/compare.py
@@ -6,7 +6,7 @@ def convert_elementwise(ctx, op):
     input_b = ctx.method_args[1]
     output = ctx.method_return
     input_a_trt, input_b_trt = add_missing_trt_tensors(ctx.network, [input_a, input_b])
-    input_a_trt, input_b_trt = broadcast_trt_tensors(ctx.network, [input_a_trt, input_b_trt], len(output.shape) - 1)
+    input_a_trt, input_b_trt = broadcast_trt_tensors(ctx.network, [input_a_trt, input_b_trt], max(len(input_a_trt.shape), len(input_b_trt.shape)))
     layer = ctx.network.add_elementwise(input_a_trt, input_b_trt, op)
     output._trt = layer.get_output(0)
 
@@ -58,3 +58,87 @@ def test_gt_basic():
 @add_module_test(torch.float32, torch.device('cuda'), [(1, 3, 6, 6), (1, 3, 6, 6)], enabled=trt_version() >= '7.0')
 def test_gt_basic():
     return EQ()
+
+
+class TensorGTScalar(torch.nn.Module):
+    def __init__(self, scalar):
+        super().__init__()
+        self.scalar = scalar
+
+    def forward(self, tensor):
+        return tensor > self.scalar
+
+
+@add_module_test(torch.float32, torch.device('cuda'), [(1, 3, 6, 6)], enabled=trt_version() >= '7.0')
+def test_tensor_gt_scalar():
+    return TensorGTScalar(0.1)
+
+
+class ScalarGTTensor(torch.nn.Module):
+    def __init__(self, scalar):
+        super().__init__()
+        self.scalar = scalar
+
+    def forward(self, tensor):
+        return self.scalar > tensor
+
+
+@add_module_test(torch.float32, torch.device('cuda'), [(1, 3, 6, 6)], enabled=trt_version() >= '7.0')
+def test_scalar_gt_scalar():
+    return ScalarGTTensor(0.1)
+
+
+class TensorLTScalar(torch.nn.Module):
+    def __init__(self, scalar):
+        super().__init__()
+        self.scalar = scalar
+
+    def forward(self, tensor):
+        return tensor < self.scalar
+
+
+@add_module_test(torch.float32, torch.device('cuda'), [(1, 3, 6, 6)], enabled=trt_version() >= '7.0')
+def test_tensor_lt_scalar():
+    return TensorLTScalar(0.1)
+
+
+class ScalarLTTensor(torch.nn.Module):
+    def __init__(self, scalar):
+        super().__init__()
+        self.scalar = scalar
+
+    def forward(self, tensor):
+        return self.scalar < tensor
+
+
+@add_module_test(torch.float32, torch.device('cuda'), [(1, 3, 6, 6)], enabled=trt_version() >= '7.0')
+def test_scalar_lt_tensor():
+    return ScalarLTTensor(0.1)
+
+
+class TensorEQScalar(torch.nn.Module):
+    def __init__(self, scalar):
+        super().__init__()
+        self.scalar = scalar
+
+    def forward(self, tensor):
+        return tensor == self.scalar
+
+
+@add_module_test(torch.float32, torch.device('cuda'), [(1, 3, 6, 6)], enabled=trt_version() >= '7.0')
+def test_tensor_eq_scalar():
+    return TensorEQScalar(0.1)
+
+
+class ScalarEQTensor(torch.nn.Module):
+    def __init__(self, scalar):
+        super().__init__()
+        self.scalar = scalar
+
+    def forward(self, tensor):
+        return self.scalar == tensor
+
+
+@add_module_test(torch.float32, torch.device('cuda'), [(1, 3, 6, 6)], enabled=trt_version() >= '7.0')
+def test_scalar_eq_tensor():
+    return ScalarEQTensor(0.1)
