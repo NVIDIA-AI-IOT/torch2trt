9c11697d5065450692331d15efb653697e413ad5
John Welsh <jaybdub@users.noreply.github.com>
2024-05-01 10:45:24 -0700

    migrate existing pytorch tests to isolated directory

--- tests/feature_tests/test_save_load.py
+++ tests/feature_tests/test_save_load.py
@@ -1,21 +1,21 @@
-from torch2trt import *
+import pytest
+import torch2trt
 import torchvision
 import torch
-from .segmentation import deeplabv3_resnet50
 
 
-if __name__ == '__main__':
-    model = deeplabv3_resnet50().cuda().eval().half()
+def test_save_load():
+    model = torch.nn.Conv2d(3, 3, 1).cuda().eval().half()
     data = torch.randn((1, 3, 224, 224)).cuda().half()
     
     print('Running torch2trt...')
-    model_trt = torch2trt(model, [data], fp16_mode=True, max_workspace_size=1<<25)
+    model_trt = torch2trt.torch2trt(model, [data], fp16_mode=True, max_workspace_size=1<<25)
 
     print('Saving model...')
     torch.save(model_trt.state_dict(), '.test_model.pth')
 
     print('Loading model...')
-    model_trt_2 = TRTModule()
+    model_trt_2 = torch2trt.TRTModule()
     model_trt_2.load_state_dict(torch.load('.test_model.pth'))
 
     assert(model_trt_2.engine is not None)
--- tests/model_tests/torchvision/test_segmentation_models.py
+++ tests/model_tests/torchvision/test_segmentation_models.py
@@ -1,6 +1,6 @@
 import torch
 import torchvision
-from torch2trt.module_test import add_module_test
+import torch2trt
 
 
 class ModelWrapper(torch.nn.Module):
@@ -9,31 +9,38 @@ class ModelWrapper(torch.nn.Module):
         self.model = model
     def forward(self, x):
         return self.model(x)['out']
-    
 
-@add_module_test(torch.float16, torch.device('cuda'), [(1, 3, 224, 224)], fp16_mode=True)  
-def deeplabv3_resnet50():
+
+def _cross_validate_module(model, shape=(224, 224)):
+    model = model.cuda().eval()
+    data = torch.randn(1, 3, *shape).cuda()
+    model_trt = torch2trt.torch2trt(model, [data])
+    data = torch.randn(1, 3, *shape).cuda()
+    out = model(data)
+    out_trt = model_trt(data)
+    assert torch.allclose(out, out_trt, rtol=1e-2, atol=1e-2)
+
+
+
+def test_deeplabv3_resnet50():
     bb = torchvision.models.segmentation.deeplabv3_resnet50(pretrained=False)
     model = ModelWrapper(bb)
-    return model
+    _cross_validate_module(model)
 
 
-@add_module_test(torch.float16, torch.device('cuda'), [(1, 3, 224, 224)], fp16_mode=True)
-def deeplabv3_resnet101():
+def test_deeplabv3_resnet101():
     bb = torchvision.models.segmentation.deeplabv3_resnet101(pretrained=False)
     model = ModelWrapper(bb)
-    return model
+    _cross_validate_module(model)
 
 
-@add_module_test(torch.float16, torch.device('cuda'), [(1, 3, 224, 224)], fp16_mode=True)
-def fcn_resnet50():
+def test_fcn_resnet50():
     bb = torchvision.models.segmentation.fcn_resnet50(pretrained=False)
     model = ModelWrapper(bb)
-    return model
+    _cross_validate_module(model)
 
 
-@add_module_test(torch.float16, torch.device('cuda'), [(1, 3, 224, 224)], fp16_mode=True)
-def fcn_resnet101():
+def test_fcn_resnet101():
     bb = torchvision.models.segmentation.fcn_resnet101(pretrained=False)
     model = ModelWrapper(bb)
-    return model
+    _cross_validate_module(model)
